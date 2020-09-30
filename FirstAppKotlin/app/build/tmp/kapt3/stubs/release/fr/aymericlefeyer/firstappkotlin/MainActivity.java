55a639c6703e00a8237031a74cccf77a1f6f97fe
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2020-09-30 13:35:39 +0200

    before start

---
+++ FirstAppKotlin/app/build/tmp/kapt3/stubs/release/fr/aymericlefeyer/firstappkotlin/MainActivity.java
@@ -0,0 +1,26 @@
+package fr.aymericlefeyer.firstappkotlin;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 4, 0}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0012\u0010\t\u001a\u00020\n2\b\u0010\u000b\u001a\u0004\u0018\u00010\fH\u0015R\u001a\u0010\u0003\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\b\u00a8\u0006\r"}, d2 = {"Lfr/aymericlefeyer/firstappkotlin/MainActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "n", "", "getN", "()I", "setN", "(I)V", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "app_release"})
+public final class MainActivity extends androidx.appcompat.app.AppCompatActivity {
+    private int n = 0;
+    private java.util.HashMap _$_findViewCache;
+    
+    public final int getN() {
+        return 0;
+    }
+    
+    public final void setN(int p0) {
+    }
+    
+    @androidx.annotation.RequiresApi(value = android.os.Build.VERSION_CODES.O)
+    @java.lang.Override()
+    protected void onCreate(@org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    public MainActivity() {
+        super();
+    }
+}
---
+++ roadmap.md
@@ -0,0 +1,13 @@
+# A connaitre avant de commencer
+- Activites
+- Savoir utiliser les fragments
+
+# Naviguer entre nos fragments
+Jetpack
+
+# Afficher les images
+Glide
+
+# Faire les appels API
+Retrofit
+

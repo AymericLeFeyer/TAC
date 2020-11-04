b1271cb472df6c08104bfbf62b5b6dfa4c5dad07
Charles Tacquet <ctacquet@outlook.fr>
2020-11-04 17:52:47 +0100

    Petit avancement

--- Listify/app/src/main/java/fr/leftac/listify/fragment/launcherFragment.java
+++ Listify/app/src/main/java/fr/leftac/listify/fragment/launcherFragment.java
@@ -2,7 +2,12 @@ package fr.leftac.listify.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,67 +22,27 @@ import fr.leftac.listify.R;
 import fr.leftac.listify.controller.Controller;
 import fr.leftac.listify.model.api.TokenManager;
 import fr.leftac.listify.model.pojo.Track;
-import fr.leftac.listify.view.MainActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link launcherFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class launcherFragment extends Fragment implements Controller.TrackCallbackListener {
     private Button tokenButton, searchButton;
     private Controller controller;
+    private NavController navController;
 
     private List<Track> tracks;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public launcherFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragmentMain.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static launcherFragment newInstance(String param1, String param2) {
-        launcherFragment fragment = new launcherFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_launcher, container, false);
 
         // Views
-        tokenButton = findViewById(R.id.tokenButton);
-        searchButton = findViewById(R.id.searchButton);
+        tokenButton = view.findViewById(R.id.tokenButton);
+        searchButton = view.findViewById(R.id.searchButton);
 
         // Init variables
         tracks = new ArrayList<>();
@@ -94,11 +59,13 @@ public class launcherFragment extends Fragment implements Controller.TrackCallba
 
         searchButton.setOnClickListener(v -> {
             tracks = new ArrayList<>();
-            controller = new Controller(MainActivity.this);
+            controller = new Controller(this);
             controller.searchTracks("orelsan");
         });
 
-        return inflater.inflate(R.layout.fragment_launcher, container, false);
+        navController = Navigation.findNavController(getActivity(), R.id.navHostFragment);
+
+        return view;
     }
 
     @Override
@@ -112,6 +79,6 @@ public class launcherFragment extends Fragment implements Controller.TrackCallba
         for (int i = 0; i < tracks.size(); i++) {
             msg.append("Track n°").append(i + 1).append(" : ").append(tracks.get(i).getName()).append('\n');
         }
-        Toast.makeText(this, msg.toString(), Toast.LENGTH_LONG).show();
+        Toast.makeText(getActivity(), msg.toString(), Toast.LENGTH_LONG).show();
     }
 }
--- Listify/app/src/main/java/fr/leftac/listify/view/MainActivity.java
+++ Listify/app/src/main/java/fr/leftac/listify/view/MainActivity.java
@@ -5,6 +5,12 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,5 +26,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
+        NavController navController = navHostFragment.getNavController();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
     }
 }

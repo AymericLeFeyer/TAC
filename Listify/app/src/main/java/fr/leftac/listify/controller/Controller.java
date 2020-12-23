3c3506d58691975edbd6d3ad17a5d56cf773334f
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2020-12-23 20:40:52 +0100

    fix duplicated in favorites

--- Listify/app/src/main/java/fr/leftac/listify/controller/Controller.java
+++ Listify/app/src/main/java/fr/leftac/listify/controller/Controller.java
@@ -13,6 +13,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import fr.leftac.listify.model.api.ApiManager;
 import fr.leftac.listify.model.api.TokenManager;
@@ -77,7 +78,6 @@ public class Controller {
 
     public void changeTrackDetails(Track track) {
         trackCallbackListener.onNewTrack(track);
-        Log.e("controller", track.getName());
     }
 
     public void updateArtist(Artist artist) {
@@ -168,8 +168,36 @@ public class Controller {
         });
     }
 
+    public void updateTrack(Track track) {
+        apiManager.getSpotifyApi().getTrack(TokenManager.getToken(), track.getId()).enqueue(new Callback<Object>() {
+            @Override
+            public void onResponse(@NotNull Call<Object> call, @NotNull Response<Object> response) {
+                if (response.body() != null) {
+                    // Parse the response
+                    JsonObject responseBody = parser.parse(new Gson().toJson(response.body())).getAsJsonObject();
+
+                    // Update tracks
+                    JsonObject tracks = responseBody.getAsJsonObject("tracks");
+                    JsonArray items = tracks.getAsJsonArray("items");
+
+
+                } else {
+                    Log.e("searchError", "response.body is null");
+                }
+
+            }
+
+            @Override
+            public void onFailure(@NotNull Call<Object> call, @NotNull Throwable t) {
+
+            }
+        });
+
+    }
+
 
     public void saveTrackToBDD(Track track) {
+        Log.e("controller", track.getName());
 
         try (Realm realm = Realm.getDefaultInstance()) {
             realm.executeTransaction(realm1 -> {
@@ -202,10 +230,29 @@ public class Controller {
         try (Realm realm = Realm.getDefaultInstance()) {
             realm.executeTransaction(realm1 -> {
                 RealmResults<Track> results = realm1.where(Track.class).equalTo("favorite", true).findAll();
-                savedTracks.addAll(realm1.copyFromRealm(results));
+
+
+                for (Track t : results) {
+                    if (!isAlreadyIn(t, savedTracks)) {
+
+                        savedTracks.add(realm1.copyFromRealm(t));
+
+                    }
+                }
+
             });
         }
         return savedTracks;
+    }
+
+    public boolean isAlreadyIn(Track t, List<Track> list) {
+        for (Track track : list) {
+            if (track.getId().equals(t.getId())) {
+                return true;
+            }
+        }
+        return false;
+
     }
 
     public boolean isFavorite(Track t) {
